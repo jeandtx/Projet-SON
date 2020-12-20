0354494a87276d867fb158d09ccae76f222eb7ed
jeandtx <jean.doutriaux@gmail.com>
2020-12-20 19:23:18 +0100

    J'ai fini l'interface graph

---
+++ interface_graph.py
@@ -0,0 +1,87 @@
+from tkinter import *
+from lecture_partition import *
+from functools import partial
+
+
+def function():
+    print("work")
+
+
+# remplacer la fonction par le script de lecture partition en le mettant en fonction
+
+
+# parameters window
+window = Tk()
+window.title("Projet Son")
+window.geometry('1080x750')
+window.minsize(750, 500)
+window.iconbitmap('logo.ico')
+window.config(background='#ABEBC6')
+
+# titre(texte)
+welcome = Label(window, text="Choisissez votre morceau", font=('', 40), bg='#ABEBC6', fg='#21618C', pady=40)
+welcome.pack(side=TOP)
+
+# boites
+morceaux = Frame(window, bg='#ABEBC6')
+gauche = Frame(morceaux, bg='#ABEBC6')
+droite = Frame(morceaux, bg='#ABEBC6')
+
+# boutons
+joyeux_anniversaire = Button(gauche, text="Joyeux anniversaire", font=('', 20), bg='#ABEBC6', fg='#145A32',
+                             command=partial(play, 2))
+joyeux_anniversaire.pack(fill=X, pady=10)
+au_clair_de_la_lune = Button(gauche, text="Au clair de la lune", font=('', 20), bg='#ABEBC6', fg='#145A32',
+                             command=partial(play, 4))
+au_clair_de_la_lune.pack(fill=X, pady=10)
+vive_le_vent = Button(gauche, text="Vive le vent", font=('', 20), bg='#ABEBC6', fg='#145A32', command=partial(play, 6))
+vive_le_vent.pack(fill=X, pady=10)
+frere_jacques = Button(gauche, text="Frères Jacques", font=('', 20), bg='#ABEBC6', fg='#145A32',
+                       command=partial(play, 8))
+frere_jacques.pack(fill=X, pady=10)
+la_claire_fontaine = Button(gauche, text="À la claire fontaine", font=('', 20), bg='#ABEBC6', fg='#145A32',
+                            command=partial(play, 10))
+la_claire_fontaine.pack(fill=X, pady=10)
+au_feu_les_pompiers = Button(gauche, text="Au feu les pompiers", font=('', 20), bg='#ABEBC6', fg='#145A32',
+                             command=partial(play, 12))
+au_feu_les_pompiers.pack(fill=X, pady=10)
+fais_dodo = Button(droite, text="Fait dodo cola mon p'tit frère", font=('', 20), bg='#ABEBC6', fg='#145A32',
+                   command=partial(play, 14))
+fais_dodo.pack(fill=X, pady=10)
+une_souris_vert = Button(droite, text="Une souris verte", font=('', 20), bg='#ABEBC6', fg='#145A32',
+                         command=partial(play, 16))
+une_souris_vert.pack(fill=X, pady=10)
+a_vous_dirais_je_maman = Button(droite, text="À vous dirais-je Maman", font=('', 20), bg='#ABEBC6', fg='#145A32',
+                                command=partial(play, 18))
+a_vous_dirais_je_maman.pack(fill=X, pady=10)
+le_bon_roi = Button(droite, text="Le bon roi Dagobert", font=('', 20), bg='#ABEBC6', fg='#145A32',
+                    command=partial(play, 20))
+le_bon_roi.pack(fill=X, pady=10)
+bateau_sur_eau = Button(droite, text="Bâteau sur l'eau", font=('', 20), bg='#ABEBC6', fg='#145A32',
+                        command=partial(play, 22))
+bateau_sur_eau.pack(fill=X, pady=10)
+la_mere_michel = Button(droite, text="C'est la mère Michel", font=('', 20), bg='#ABEBC6', fg='#145A32',
+                        command=partial(play, 24))
+la_mere_michel.pack(fill=X, pady=10)
+
+# image
+w = 500
+h = 500
+image = PhotoImage(file='note.png')
+canvas = Canvas(morceaux, width=w, height=h, bg='#ABEBC6', bd=0, highlightthickness=0)
+canvas.create_image(w / 2, h / 2, image=image)
+
+morceaux.pack(expand=YES)
+gauche.pack(side=LEFT)
+canvas.pack(side=LEFT)
+droite.pack(side=RIGHT)
+
+# dernier bouton pour creer un morceau
+bas = Frame(window, bg='#ABEBC6')
+nvx_morceau = Button(bas, text="Vous pouvez aussi créér votre propre morceau !", font=('', 15),
+                     bg='#ABEBC6', fg='#145A32', command=function)
+nvx_morceau.pack(fill=X)
+bas.pack(expand=YES)
+
+# display window
+window.mainloop()
--- lecture_partition.py
+++ lecture_partition.py
@@ -1,11 +1,5 @@
-"""partition = open("partitions.txt", "r")
-content = partition.readlines()
-for i in range(0, len(content)):
-    print(content[i])
-partition.close()"""
-
-from fct_son_internet import *
 from fct_son import *
+
 notes = {'DO': 264, 'RE': 297, 'MI': 330, 'FA': 352, 'SOL': 396, 'LA': 440, 'SI': 495, 'Z': 0}
 durations = {'c': 0.125, 'n': 0.25, 'b': 0.5, 'r': 1}
 p = "extend the duration of the previous note by 50 percent"
@@ -18,27 +12,28 @@ def in_dic(string, dic):
     return False
 
 
-partition = open("partitions.txt", "r")
-content = partition.readlines()
-song = int(input("Which song? give the line number : ")) - 1
-reader = ""
-frequency = 0
-duration = 0
-for i in range(0, len(content[song])):
-    reader += content[song][i]
-    if in_dic(reader, notes):
-        frequency = notes[reader]
-        reader = ""
-    elif in_dic(reader, durations):
-        duration = durations[reader]
-        reader = ""
-    elif reader == " ":
-        reader = ""
-    elif reader == "p":
-        duration *= 1.5
-        reader = ""
-    elif frequency != 0 and duration != 0:
-        sound(frequency, duration)
-        frequency = 0
-        duration = 0
-partition.close()
+def play(song):
+    partition = open("partitions.txt", "r")
+    content = partition.readlines()
+    song -= 1
+    reader = ""
+    frequency = 0
+    duration = 0
+    for i in range(0, len(content[song])):
+        reader += content[song][i]
+        if in_dic(reader, notes):
+            frequency = notes[reader]
+            reader = ""
+        elif in_dic(reader, durations):
+            duration = durations[reader]
+            reader = ""
+        elif reader == " ":
+            reader = ""
+        elif reader == "p":
+            duration *= 1.5
+            reader = ""
+        elif frequency != 0 and duration != 0:
+            sound(frequency, duration)
+            frequency = 0
+            duration = 0
+    partition.close()
