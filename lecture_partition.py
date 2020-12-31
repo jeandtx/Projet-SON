7c891f76fb8d3426c8086c12718b5500a8f2cd63
joknarf <joknarf@free.fr>
2020-12-31 15:18:49 +0100

    fix "p" tag reading

--- lecture_partition.py
+++ lecture_partition.py
@@ -1,4 +1,4 @@
-# Projet Son: Paul Jouvenceau, Saul Delmotte, Jean Doutriaux
+# Projet Son: Paul Jouvanceau, Saul Delmotte, Jean Doutriaux
 # Le fichier comporte plusieurs fonctions utilisé dans l'interface graphique
 
 from fct_son import *
@@ -18,7 +18,7 @@ def in_dic(string, dic):
 
 # Cette fonction sert à lire la partition et la jouer
 def play(song):
-    partition = open("/Users/jeandtx/Documents/Projet SONN/partitions.txt", "r")
+    partition = open("partitions.txt", "r")
     content = partition.readlines()
     user_partition = open("user_partition.txt")
     content.append(user_partition.readline())
@@ -36,13 +36,12 @@ def play(song):
         elif in_dic(reader, durations):
             duration = durations[reader]
             reader = ""
-        elif reader == " ":
-            reader = ""
-        if reader == "p":
-            duration *= 1.5
+            if len(content[song]) > j and content[song][j] == "p":
+                duration *= 1.5
+        elif reader == " " or reader == "p":
             reader = ""
         # Quand chaque argument à été trouvé on peut appeler la fonction qui joue le son et reinitialiser ces variables
-        if frequency != 0 and duration != 0 and content[song][j] != "p":
+        if frequency != 0 and duration != 0:
             sound(frequency, duration)
             frequency = 0
             duration = 0
@@ -51,7 +50,7 @@ def play(song):
 
 # Cette fonction ajoute une nouvelle melodie à la partition
 def new_song():
-    partition = open("/Users/jeandtx/Documents/Projet SONN/user_partition.txt", "w")
+    partition = open("user_partition.txt", "w")
     user = input("Insert your new song: ")
     partition.write(user)
     play(27)
