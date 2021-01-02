552e0bcbcff900d0b073dc1be4c16d285e1a2f58
jeandtx <jean.doutriaux@gmail.com>
2021-01-02 16:05:52 +0100

    input de nouvelle partition dans sa propre interface. Prblm: il faut fermer le programme pour qu'il joue ce qu'on vient d'ecrire

--- lecture_partition.py
+++ lecture_partition.py
@@ -2,9 +2,11 @@
 # Le fichier comporte plusieurs fonctions utilisées dans l'interface graphique
 
 from fct_son import *
+from tkinter import *
 
 frequencies = {'DO': 264, 'RE': 297, 'MI': 330, 'FA': 352, 'SOL': 396, 'LA': 440, 'SI': 495, 'Z': 0}
 durations = {'c': 0.125, 'n': 0.25, 'b': 0.5, 'r': 1}
+
 
 # Cette fonction sert à lire la partition et la jouer
 def play(song):
@@ -32,9 +34,24 @@ def play(song):
 # Cette fonction ajoute une nouvelle melodie à la partition
 def new_song():
     partition = open("user_partition.txt", "w")
-    user = input("Insert your new song: ")
-    partition.write(user)
-    play(27)
+    instructions = Tk()
+    instructions.title("Create your partition")
+    instructions.geometry("600x300")
+    rules = Label(instructions, text="Write each notes in capital letters followed by the duration in small seperate "
+                                     "all by a space. \nSilence are Z and for durations put the first letter of the "
+                                     "silence's name (ex: croche = c) \n And on ONE line !")
+    rules.pack(expand=YES)
+    text = Text(instructions, height=2)
+    text.pack(expand=YES)
+
+    def getTextInput():
+        result = text.get("1.0", "end")
+        partition.write(result)
+        instructions.destroy()
+
+    enter = Button(instructions, text="Enter", command=getTextInput)
+    enter.pack(expand=YES)
+    instructions.mainloop()
 
 
 # Cette fonction sert à executer deux fonctions au lieu d'une quand on appuie sur un bouton de l'interface graph
